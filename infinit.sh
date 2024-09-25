394b9f1c786a50fef417a359d1e93d6957749df1
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-09-25 14:30:13 +0800

    Update infinit.sh

--- infinit.sh
+++ infinit.sh
@@ -102,8 +102,7 @@ function deploy_contract() {
 
     sleep 5
     echo
-
-    mkdir -p src/scripts
+    # Removing old deployUniswapV3Action script if exists
     rm -rf src/scripts/deployUniswapV3Action.script.ts
 
     cat <<EOF > src/scripts/deployUniswapV3Action.script.ts
@@ -112,13 +111,22 @@ import type { z } from 'zod'
 
 type Param = z.infer<typeof actions['init']['paramsSchema']>
 
+// TODO: Replace with actual params
 const params: Param = {
+  // Native currency label (e.g., ETH)
   "nativeCurrencyLabel": 'ETH',
+
+  // Address of the owner of the proxy admin
   "proxyAdminOwner": '$WALLET',
+
+  // Address of the owner of factory
   "factoryOwner": '$WALLET',
+
+  // Address of the wrapped native token (e.g., WETH)
   "wrappedNativeToken": '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 }
 
+// Signer configuration
 const signer = {
   "deployer": "$ACCOUNT_ID"
 }
